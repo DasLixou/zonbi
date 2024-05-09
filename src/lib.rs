6802a98f2ab54b89fdca345e4a562b85b92885ef
DasLixou <einlixou@gmail.com>
2024-05-09 19:33:13 +0200

    Fix docs and increment patch version

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,35 @@
-///! # Zonbi
-///!
-///! This is an experiment to make it possible to type-erase non-`'static` types.
-///!
-///! ## How it works
-///!
-///! With `#[derive(Zonbi)]`, the type gets an implementation for getting a version of the type where all lifetimes are replaced with the given one.
-///! Manual implementation is unsafe because the user must assure that the `Casted` type is the same as the one of the implementer.
-///!
-///! This is used in `ZonbiId`, a wrapper around `TypeId`, which different from its inside value, has the additional definition of behaviour for non-`'static` types.
-///! `ZonbiId` is unique for every type, **excluding** its lifetimes.
-///! Under the hood, it just uses the `Zonbi` trait to get the `'static` version of the type and gets its `TypeId`.
-///!
-///! To hold such type-erased value inside for example a box, you can create a `Cage<'life, Z>` of the zonbi `Z` and then hold that in a `dyn AnyZonbi<'life>` with the associated minimal lifetime.
-///! Every zonbi that lives for at least `'life` can be upcasted into this trait and downcasted back into it with all the lifetimes being this mininal `'life` one.
-///!
-///! ## Example
-///!
-///! ```ignore
-///! let mut type_map: HashMap<ZonbiId, Box<dyn AnyZonbi<'a>>> = HashMap::new();
-///!
-///! let id = ZonbiId::of::<MyStruct>();
-///! type_map.insert(id, Cage::new(Box::new(MyStruct { my_reference: &val })));
-///!
-///! let r: &MyStruct<'a> = type_map[&id].downcast_ref::<MyStruct<'a>>().unwrap();
-///! ```
-///!
-///! _This is a broken down snippet of the [`type_map` example](https://github.com/DasLixou/zonbi/blob/master/examples/type_map.rs)._
-///!
-///! ## License
-///!
-///! Dual-licensed under [`Apache-2.0`](LICENSE-APACHE) and [`MIT`](LICENSE-MIT)
+//! # Zonbi
+//!
+//! This is an experiment to make it possible to type-erase non-`'static` types.
+//!
+//! ## How it works
+//!
+//! With `#[derive(Zonbi)]`, the type gets an implementation for getting a version of the type where all lifetimes are replaced with the given one.
+//! Manual implementation is unsafe because the user must assure that the `Casted` type is the same as the one of the implementer.
+//!
+//! This is used in [`ZonbiId`], a wrapper around [`TypeId`], which different from its inside value, has the additional definition of behaviour for non-`'static` types.
+//! `ZonbiId` is unique for every type, **excluding** its lifetimes.
+//! Under the hood, it just uses the [`Zonbi`] trait to get the `'static` version of the type and gets its `TypeId`.
+//!
+//! To hold such type-erased value inside for example a box, you can create a [`Cage<'life, Z>`] of the zonbi `Z` and then hold that in a [`dyn AnyZonbi<'life>`][AnyZonbi] with the associated minimal lifetime.
+//! Every zonbi that lives for at least `'life` can be upcasted into this trait and downcasted back into it with all the lifetimes being this mininal `'life` one.
+//!
+//! ## Example
+//!
+//! ```ignore
+//! let mut type_map: HashMap<ZonbiId, Box<dyn AnyZonbi<'a>>> = HashMap::new();
+//!
+//! let id = ZonbiId::of::<MyStruct>();
+//! type_map.insert(id, Cage::new(Box::new(MyStruct { my_reference: &val })));
+//!
+//! let r: &MyStruct<'a> = type_map[&id].downcast_ref::<MyStruct<'a>>().unwrap();
+//! ```
+//!
+//! _This is a broken down snippet of the [`type_map` example](https://github.com/DasLixou/zonbi/blob/master/examples/type_map.rs)._
+//!
+//! ## License
+//!
+//! Dual-licensed under [`Apache-2.0`](LICENSE-APACHE) and [`MIT`](LICENSE-MIT)
 use std::{any::TypeId, marker::PhantomData};
 
 pub use zonbi_macros::Zonbi;
@@ -134,7 +134,7 @@ impl<'life> dyn AnyZonbi<'life> {
 ///
 /// This is done because it forbids to have a lower-lifetimed mutable reference to the zonbi,
 /// preventing a possible use-after-free reference.
-/// See [this comment][https://internals.rust-lang.org/t/type-erasing-non-static-types/20785/2] for more.
+/// See [this comment](https://internals.rust-lang.org/t/type-erasing-non-static-types/20785/2) for more.
 ///
 /// [`dyn AnyZonbi`]: AnyZonbi
 pub struct Cage<'life, Z: Zonbi<'life>> {
